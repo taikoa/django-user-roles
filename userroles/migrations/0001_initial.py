b2b90faae90973b4aa0b22f69f02366eca6b581e
Tijs Teulings <tijs@automatique.nl>
2013-07-31 15:48:13 +0200

    fixing the migration to go with the rest of the 1.5 user model changes

--- userroles/migrations/0001_initial.py
+++ userroles/migrations/0001_initial.py
@@ -4,6 +4,17 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+try:
+    from django.contrib.auth import get_user_model
+except ImportError: # django < 1.5
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
+user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
+
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
@@ -11,7 +22,7 @@ class Migration(SchemaMigration):
         # Adding model 'UserRole'
         db.create_table('userroles_userrole', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='role', unique=True, to=orm['auth.User'])),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='role', unique=True, to=orm[user_orm_label])),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('child', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
         ))
@@ -38,8 +49,8 @@ class Migration(SchemaMigration):
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
+        user_model_label: {
+            'Meta': {'object_name': User.__name__, 'db_table': "'%s'" % User._meta.db_table},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
@@ -66,7 +77,7 @@ class Migration(SchemaMigration):
             'child': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'role'", 'unique': 'True', 'to': "orm['auth.User']"})
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'role'", 'unique': 'True', 'to': "orm['%s']" % user_orm_label})
         }
     }
 
